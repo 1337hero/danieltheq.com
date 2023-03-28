1b09a0d327f8ff673aa31cbbc73e3ecfe7184f14
Mike Key <mike@1337hero.com>
2023-03-28 13:48:37 -0600

    first build

---
+++ config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "siteTitle": "Daniel Quijano",
+    "author": "1337 Hero",
+    "description": "",
+
+    /*-- Intro --*/
+    "avatar": "./img/dq.webp",
+    "pageTitle": "Daniel Quijano",
+    "subTitle": "",
+    "tagline": "I help Real Estate Investors close deals 💰",
+
+    /*-- Social Media Links --*/
+    "youtube": "https://www.youtube.com/@Daniel.TheQOfficial",
+    "instagram": "https://www.instagram.com/daniel.theq/",
+    "facebook": "https://www.facebook.com/Daniel.TheQOfficial",
+    "linkedin": "#",
+    "twiiter": "https://twitter.com/",
+    "tiktok": "https://www.tiktok.com/@daniel.theq",
+
+    /*-- Connect Buttons --*/
+    "calendly": "#"
+}
